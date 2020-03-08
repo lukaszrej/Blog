f6d07e350b15c72a0d3f690af891fd9a9dc44b3f
Lukasz Rej <rejlukasz@gmail.com>
2020-03-08 20:26:20 +0100

    Cleaning. Added styles directory. (#1)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog Pro`,
+    description: `Blog. Build with Gatsby JS.`,
+    author: `Autor Sukcesu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
