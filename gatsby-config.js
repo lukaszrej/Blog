c626f6eb77a276259970990df938ca50abe451a1
Lukasz Rej <rejlukasz@gmail.com>
2020-03-25 22:27:47 +0100

    Fonts changed (typography.js). Reset.scss adjusted. (#3)
    Co-authored-by: Lukasz Rej <lukasz.rej@miinto.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
         url: "https://api.graphcms.com/simple/v1/swapi",
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
