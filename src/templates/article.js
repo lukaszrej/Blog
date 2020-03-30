dc8469a02b8eac5f846c92cd1260266a5d66bdea
Lukasz Rej <lukasz.rej@miinto.com>
2020-03-30 22:02:13 +0200

    React-markdown package added.

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
+import ReactMarkdown from "react-markdown";
 
 import Layout from '../components/layout';
 
@@ -8,7 +9,7 @@ const ArticleTemplate = ({ data }) => (
         <main className="p-main">
             <h2 className='p-article__title'>{data.strapiArticle.title}</h2>
             <p className='p-article__date'>by <Link to={`/authors/User_${data.strapiArticle.author.id}`}>{data.strapiArticle.author.username}</Link></p>
-            <p className='p-article__content'>{data.strapiArticle.content}</p>
+            <ReactMarkdown className='p-article__content' source={data.strapiArticle.content} />
             <Link to="/">Go Back</Link>
         </main>
     </Layout>
