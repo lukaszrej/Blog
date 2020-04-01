695ee04ee821111f65685adcecbeb90e7bfecc64
Lukasz Rej <rejlukasz@gmail.com>
2020-04-01 19:55:07 +0200

    Styles and structure changes. (#6)
    Co-authored-by: Lukasz Rej <lukasz.rej@miinto.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => (
     <footer className='p-footer'>
-        <div className='p-footer__content'>© {new Date().getFullYear()} Autor Sukcesu</div>
+        <div className='p-footer__content'>© 2020 Łukasz Rej </div>
     </footer>
 );
 
